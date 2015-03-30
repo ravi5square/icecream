5f64b48c0d83ef04c915243a8121a4ea560709df
ravi5square <ravi5square@gmail.com>
2015-03-30 16:06:18 +0530

    Add contact-us.php

---
+++ contact-us.php
@@ -0,0 +1,650 @@
+<html dir="ltr" lang="en-US"><head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>final year projects in bangalore | ieee project institutes in bangalore| real time academic projects</title>
+<meta name="title" content="final year projects in bangalore | ieee project institutes in bangalore| real time academic projects" />
+
+<meta name="description" content="projectavenue well organised in services. specialized in final year projects bangalore, ieee academic projects bangalore, engineering projects bangalore, real time academic projects. " />
+
+<link rel="author" href="https://plus.google.com/114850051101668417009" />
+<meta name="robots" content="index,follow" />
+<LINK REL="SHORTCUT ICON" HREF="favicon.png" />
+<meta name="revisit-after" content="5 days"/>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-52922947-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+    <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+
+    <link rel="stylesheet" href="style.css" media="screen">
+
+    <link rel="stylesheet" href="form.css"  media="screen" />
+
+    <link rel="stylesheet" href="new.css"  media="screen" /> 
+
+    <script src="jquery.js"></script>
+
+    <script src="script.js"></script>
+
+<style>
+
+.art-content .art-postcontent-0 .layout-item-0 { border-top-width:0px;border-top-style:solid;border-top-color:#679843;margin-top: 10px;margin-bottom: 10px;  }
+
+.art-content .art-postcontent-0 .layout-item-1 {  border-collapse: separate;  }
+
+.art-content .art-postcontent-0 .layout-item-2 { padding-right: 10px;padding-left: 10px;  }
+
+.art-content .art-postcontent-0 .layout-item-3 { border-top-width:1px;border-top-style:solid;border-top-color:#679843;margin-top: 10px;  }
+
+.art-content .art-postcontent-0 .layout-item-4 { border-spacing: 5px 0px; border-collapse: separate;  }
+
+.art-content .art-postcontent-0 .layout-item-5 { border-top-style:solid;border-right-style:solid;border-bottom-style:solid;border-left-style:solid;border-top-width:1px;border-right-width:1px;border-bottom-width:1px;border-left-width:1px;border-top-color:#679843;border-right-color:#679843;border-bottom-color:#679843;border-left-color:#679843; color: #141819; background:  #D4E7D9; padding: 10px; border-radius: 30px;  }
+
+.art-content .art-postcontent-0 .layout-item-6 { color: #141819; background: #51A1CD;background:  rgba(81, 161, 205, 0.38); padding-top: 3px;padding-right: 25px;padding-bottom: 3px;padding-left: 25px; vertical-align: top; border-radius: 20px;  }
+
+.ie7 .post .layout-cell {border:none !important; padding:0 !important; }
+
+.ie6 .post .layout-cell {border:none !important; padding:0 !important; }
+
+.style4 {
+
+	color: #CC3300;
+
+	font-weight: bold;
+
+}
+
+.style5 {color: #CC3300}
+
+.style6 {font-size: 18}
+
+.style7 {font-size: 24px}
+
+.style8 {color: #FF0066}
+
+.style17 {font-size: 16px;
+	font-weight: bold;
+	color: #0000FF;
+}
+.style25 {color: #0000FF;
+	font-weight: bold;
+}
+</style>
+
+<script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
+
+	<script type="text/javascript" src="js/jquery.validate.min.js"></script>
+	<script src="Scripts/swfobject_modified.js" type="text/javascript"></script>
+<script>jQuery(function($) {
+
+    'use strict';
+
+    var slideContainer = $(".art-slidecontainer1292687930_scholarship"), tmp;
+
+
+
+    if ($.fn.slider && slideContainer.length) {
+
+        var inner = $(".art-slider-inner", slideContainer);
+
+        var helper = null;
+
+        
+
+        
+
+        inner.children().eq(0).addClass("active");
+
+        slideContainer.slider({
+
+            pause: 2600,
+
+            speed: 600,
+
+            repeat: true,
+
+            animation: "fade",
+
+            direction: "next",
+
+            navigator: ".art-slidenavigator1292687930_scholarship",
+
+            helper: helper
+
+                    });
+
+    }
+
+});
+
+    </script>
+
+<script type="text/javascript">
+
+	$(document).ready(function(){
+
+		$("#require").validate({
+
+			debug: false,
+
+			rules: {
+
+				name: "required",
+
+				email: {
+
+					required: true,
+
+					email: true
+
+				}
+
+			},
+
+			messages: {
+
+				name: "Enter your Name.",
+
+				email: "Valid email required.",
+
+			},
+
+			submitHandler: function(form) {
+
+				// do other stuff for a valid form
+
+				$.post('mail.php', $("#require").serialize(), function(data) {
+
+					$('#results').html(data);
+
+				});
+
+			}
+
+		});
+
+	});
+
+</script>
+
+<script language="javascript">
+
+function valid()
+
+{
+
+    
+
+    var a=document.require.name.value;
+
+	var c=document.require.mobile.value;
+
+	
+
+	var d=document.require.chk1.checked;
+
+	var e=document.require.chk2.checked;
+
+	var f=document.require.chk3.checked;
+
+	
+
+	
+
+	
+
+	 if(a=="")
+
+    {
+
+        alert("Please enter the Name");
+
+			document.require.name.focus();
+
+        return false;
+
+    }
+
+	
+
+	var emailfilter=/^\w+[\+\.\w-]*@([\w-]+\.)*\w+[\w-]*\.([a-z]{2,4}|\d+)$/i
+
+    var b=emailfilter.test(document.require.email.value);
+
+    if(b==false)
+
+    {
+
+        alert("Please enter a valid mail Id");
+
+	document.require.email.focus();
+
+        return false;
+
+    }
+
+    
+
+	
+
+   
+
+	 if(c=="")
+
+    {
+
+        alert("Please enter the Phone number");
+
+			document.require.mobile.focus();
+
+        return false;
+
+    }
+
+	 if(c.length!=10)
+
+    {
+
+        alert("Please enter the Correct Phone number");
+
+			document.require.mobile.focus();
+
+        return false;
+
+    }
+
+	if(isNaN(c))
+
+    {
+
+        alert("Please enter the Correct Phone number");
+
+			document.require.mobile.focus();
+
+        return false;
+
+    }
+
+	if((d==true)||(e==true)||(f==true)){
+
+	
+
+	}
+
+	
+
+    else
+
+    {
+
+        alert("Select any One Requirement");
+
+	return false;
+
+     }
+
+	 return true;
+
+}
+
+</script>
+
+<script language="JavaScript1.2">
+
+
+
+
+
+function neon(){
+
+
+
+//Change all letters to base color
+
+if (n==0){
+
+for (m=0;m<message.length;m++)
+
+//eval("document.all.neonlight"+m).style.color=neonbasecolor
+
+crossref(m).style.color=neonbasecolor
+
+}
+
+
+
+//cycle through and change individual letters to neon color
+
+crossref(n).style.color=neontextcolor
+
+
+
+if (n<message.length-1)
+
+n++
+
+else{
+
+n=0
+
+clearInterval(flashing)
+
+setTimeout("beginneon()",2000)
+
+return
+
+}
+
+}
+
+
+
+function beginneon(){
+
+if (document.all||document.getElementById)
+
+flashing=setInterval("neon()",flashspeed)
+
+}
+
+beginneon()
+
+          </script>
+
+
+
+</head>
+
+<body>
+
+<div id="art-main">
+
+    <div id="art-hmenu-bg" class="art-bar art-nav clearfix">
+
+    </div>
+
+<header class="art-header clearfix">
+
+<div class="art-slidecontainerheader">
+
+    <div class="art-slider-inner" style="">
+
+<div class="art-slide-item art-slideheader0" style="">
+
+
+
+</div>
+
+<div class="art-slide-item art-slideheader1" style="">
+
+
+
+</div>
+
+<div class="art-slide-item art-slideheader2" style="">
+
+
+
+</div>
+
+<div class="art-slide-item art-slideheader3" style="">
+
+
+
+</div>
+
+<div class="art-slide-item art-slideheader4" style="">
+
+
+
+</div>
+
+<div class="art-slide-item art-slideheader5" style="">
+
+
+
+</div>
+
+<div class="art-slide-item art-slideheader6" style="">
+
+
+
+</div>
+
+<div class="art-slide-item art-slideheader7" style="">
+
+
+
+</div>
+
+
+
+    </div>
+
+</div>
+
+<div class="art-slidenavigator art-slidenavigatorheader">
+
+<a href="#" class="art-slidenavigatoritem"></a><a href="#" class="art-slidenavigatoritem"></a><a href="#" class="art-slidenavigatoritem"></a><a href="#" class="art-slidenavigatoritem"></a><a href="#" class="art-slidenavigatoritem"></a><a href="#" class="art-slidenavigatoritem"></a><a href="#" class="art-slidenavigatoritem"></a><a href="#" class="art-slidenavigatoritem"></a>
+
+</div>
+
+
+
+
+<div class="row1">
+	<div class="row1_c">
+		<div class="logo">projectavenue</div>
+		<div class="call">
+		  <p>For BE,MTech,MCA,MSc,BCA,Diploma.<br />
+		     ECE,CSE,EEE,Bulk Projects<br />
+			+91-9538304161 <br />
+	        projectavenuein@gmail.com</p>
+	  </div>
+	</div>
+</div>
+		<div class="row2">
+	<ul class="menu">
+	<li><a href="home.php" class='active'>Home</a></li><li><a href="services.php">Services</a></li><li><a href="about.php">About us</a></li><li><a href="contact-us.php">Contact us</a></li><li><a href="gallery.php"></a></li>
+	</ul>
+</div>
+		<style type="text/css">
+<!--
+.style1 {
+	font-size: 18px;
+	color: #000000;
+}
+.style2 {
+	color: #CC0000;
+	font-weight: bold;
+	font-size: 18px;
+}
+-->
+</style>
+<div class="rowx">
+	<div class="banner" id="be">
+		<div class="yellow2"></div>
+	</div>
+</div>
+
+    <div class="art-content-layout-row">
+
+    <div class="art-layout-cell layout-item-4" style="width: 20%" >
+
+        <h2>Contact Us</h2>
+
+    </div><div class="art-layout-cell layout-item-4" style="width: 20%" >
+
+        <p><br></p>
+
+    </div><div class="art-layout-cell layout-item-4" style="width: 60%" >
+
+        <p class="style5"> <span class="style6">
+
+          <span class="style7"> <span class="style8">
+
+          <marquee behavior="alternate">
+
+          <strong>            For More  Info Call Us: +91-9538304161            </strong>
+
+          </marquee>
+
+          </span> </span> </span>        </p>
+
+    </div>
+
+    </div>
+
+</div>
+
+<div class="art-content-layout-br layout-item-3">
+
+</div><div class="art-content-layout layout-item-1">
+
+    <div class="art-content-layout-row">
+
+    <div class="art-layout-cell layout-item-2" style="width: 100%" >
+
+        <p><br></p>
+
+    </div>
+
+    </div>
+
+</div>
+
+<div class="art-content-layout layout-item-4">
+
+    <div class="art-content-layout-row">
+
+    <div class="art-layout-cell layout-item-5" style="width: 33%" >
+
+        <div>
+
+        <div>
+
+        <p><span style="font-size: 14px;"><b>Address:</b></span></p>
+
+        <p>No:34 'c', 8th Cross, Near BMTC Depot,<br>
+
+        BHCS Layout, Chandra layout,<br>
+
+        Vijay Nagar, Bangalore - 560040<br>
+
+        Land Mark: Opp to Union Bank.</p>
+
+        <p>          Mobile: <span class="style4">9538304161</span><br>
+
+          email : <span class="style4">projectavenuein@gmail.com</span></p>
+
+        <hr align="center" width="1" noshade="noshade" color="#006600"></div>
+        <div align="center">
+        <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="214" height="301" id="FlashID" title="Contact Us">
+          <param name="movie" value="Contact-us.swf">
+          <param name="quality" value="high">
+          <param name="wmode" value="opaque">
+          <param name="swfversion" value="9.0.45.0">
+          <!-- This param tag prompts users with Flash Player 6.0 r65 and higher to download the latest version of Flash Player. Delete it if you don’t want users to see the prompt. -->
+          <param name="expressinstall" value="Scripts/expressInstall.swf">
+          <!-- Next object tag is for non-IE browsers. So hide it from IE using IECC. -->
+          <!--[if !IE]>-->
+          <object type="application/x-shockwave-flash" data="Contact-us.swf" width="214" height="301">
+            <!--<![endif]-->
+            <param name="quality" value="high">
+            <param name="wmode" value="opaque">
+            <param name="swfversion" value="9.0.45.0">
+            <param name="expressinstall" value="Scripts/expressInstall.swf">
+            <!-- The browser displays the following alternative content for users with Flash Player 6.0 and older. -->
+            <div>
+              <h4>Content on this page requires a newer version of Adobe Flash Player.</h4>
+              <p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" /></a></p>
+            </div>
+            <!--[if !IE]>-->
+          </object>
+          <!--<![endif]-->
+        </object></div>
+        </div>
+
+    </div><div class="art-layout-cell layout-item-6" style="width: 67%" >
+
+        <div>
+
+        <p><span style="font-weight: bold; font-size: 14px;">Google Map</span></p><br><br>
+
+        <div class="border"><iframe width="600" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.co.in/maps/ms?hl=en&amp;gl=in&amp;ie=UTF8&amp;oe=UTF8&amp;msa=0&amp;msid=202287923349968744053.0004ff78c8c2a86971b41&amp;t=m&amp;ll=12.960168,77.527084&amp;spn=0,0&amp;output=embed"></iframe><br /><small>View <a href="https://www.google.co.in/maps/ms?hl=en&amp;gl=in&amp;ie=UTF8&amp;oe=UTF8&amp;msa=0&amp;msid=202287923349968744053.0004ff78c8c2a86971b41&amp;t=m&amp;ll=12.960168,77.527084&amp;spn=0,0&amp;source=embed" style="color:#0000FF;text-align:left">ProjectAvenue</a> in a larger map</smal></div>
+
+        </div>
+
+    </div>
+
+    </div>
+
+</div>
+
+<div class="art-content-layout">
+
+    <div class="art-content-layout-row">
+
+    <div class="art-layout-cell layout-item-2" style="width: 100%" >
+
+        <p><br></p>
+
+    </div>
+
+    </div>
+
+</div>
+
+</div>
+
+                                
+
+                </article></div>
+
+                    </div>
+
+                </div>
+
+    </div>
+
+  </div>
+
+<footer class="art-footer clearfix">
+
+  <div class="art-footer-inner">
+
+<div class="art-content-layout-br layout-item-0">
+
+</div><div class="art-content-layout">
+
+    <div class="art-content-layout-row">
+
+      <div class="art-layout-cell layout-item-9" style="width: 100%">
+        <div align="center"><span class="style25">
+          <p>Final Year Projects | Academic projects | IEEE Projects | Real time Projects | Engineering projects | Application Projects | Project Training | Student projects | Bulk projects | Mini projects | Final Year BE/MCA/MTECH/BCA/Diploma Projects | Final Year CSE/ECE/EEE/IT/IS Projects: projectavenue, Bangalore </p>
+        </span>Copyright ©2013 projectavenue: All Rights Reserved</div>
+      </div>
+    </div>
+
+</div>
+
+
+
+
+    </div>
+
+</footer>
+
+
+
+</div>
+<script type="text/javascript">
+swfobject.registerObject("FlashID");
+</script>
+</body></html>
